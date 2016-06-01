e7146e16174db94238dba9b04bc0ffece063e67c
Jeroen Ooms <jeroenooms@gmail.com>
2016-06-01 14:56:06 +0200

    Typo in winlibs.R

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,4 +1,4 @@
-if (!file.exists("../windows/hiredis-0.9.2/include/hiredis.h")) {
+if (!file.exists("../windows/hiredis-0.9.2/include/hiredis/hiredis.h")) {
   if (getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/hiredis/archive/v0.9.2.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
