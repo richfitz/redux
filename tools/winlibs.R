21c2df2178f42ea218e08277bd7bd2f063c07d8b
Jeroen Ooms <jeroenooms@gmail.com>
2016-06-01 13:38:11 +0200

    Add support for Windows

---
+++ tools/winlibs.R
@@ -0,0 +1,7 @@
+if (!file.exists("../windows/hiredis-0.9.2/include/hiredis.h")) {
+  if (getRversion() < "3.3.0") setInternet2()
+  download.file("https://github.com/rwinlib/hiredis/archive/v0.9.2.zip", "lib.zip", quiet = TRUE)
+  dir.create("../windows", showWarnings = FALSE)
+  unzip("lib.zip", exdir = "../windows")
+  unlink("lib.zip")
+}
