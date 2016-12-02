19c7bfeed45dc1e2bd0f452c59a0af43be4eb869
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-12-02 16:56:44 +0000

    Simplify creation of redis commands object

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 ##' @importFrom stats setNames
 ##' @importFrom utils URLdecode capture.output modifyList str
-.onLoad <- function(libname, pkgname) {
-  x <- list2env(redis_cmds(identity), hash=TRUE)
+redis <- local({
+  x <- list2env(redis_cmds(identity), hash = TRUE)
   lockEnvironment(x)
   class(x) <- "redis_commands"
-  redis <<- x
-}
+  x
+})
