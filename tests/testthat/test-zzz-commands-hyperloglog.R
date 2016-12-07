bbaa482386db3421895ae420db9abf2d35924280
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-12-07 17:38:38 +0000

    Tests for hyperloglog group commands

---
+++ tests/testthat/test-zzz-commands-hyperloglog.R
@@ -0,0 +1,37 @@
+context("commands - ")
+
+test_that("PFADD", {
+  skip_if_no_redis()
+  con <- hiredis()
+  key <- rand_str()
+  on.exit(con$DEL(key))
+
+  expect_equal(con$PFADD(key, letters[1:7]), 1L)
+  expect_equal(con$PFCOUNT(key), 7L)
+})
+
+test_that("PFCOUNT", {
+  skip_if_no_redis()
+  con <- hiredis()
+  key <- rand_str()
+  on.exit(con$DEL(key))
+
+  expect_equal(con$PFADD(key, c("foo", "bar", "zap")), 1)
+  expect_equal(con$PFADD(key, c("foo", "bar", "zap")), 0)
+  expect_equal(con$PFADD(key, c("foo", "bar")), 0)
+  expect_equal(con$PFCOUNT(key), 3)
+})
+
+test_that("PFMERGE", {
+  skip_if_no_redis()
+  con <- hiredis()
+  key1 <- rand_str()
+  key2 <- rand_str()
+  key3 <- rand_str()
+  on.exit(con$DEL(c(key1, key2, key3)))
+
+  expect_equal(con$PFADD(key1, c("foo", "bar", "zap", "a")), 1)
+  expect_equal(con$PFADD(key2, c("a", "b", "c", "foo")), 1)
+  con$PFMERGE(key3, c(key1, key2))
+  expect_equal(con$PFCOUNT(key3), 6)
+})
