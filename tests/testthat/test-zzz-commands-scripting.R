34461c40fd4fec37616b58194e5fe28701faf592
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-12-12 21:48:15 +0000

    Tests for the scripting interface
    Basic because this is not that fleshed out in the Redis docs

---
+++ tests/testthat/test-zzz-commands-scripting.R
@@ -0,0 +1,40 @@
+context("commands - scripting")
+
+test_that("EVAL", {
+  skip_if_no_redis()
+  con <- hiredis()
+
+  res <- con$EVAL("return {KEYS[1],KEYS[2],ARGV[1],ARGV[2]}", 2,
+                  c("key1", "key2"), c("first", "second"))
+  expect_equal(res, list("key1", "key2", "first", "second"))
+})
+
+test_that("EVALSHA/SCRIPT LOAD", {
+  skip_if_no_redis()
+  con <- hiredis()
+  key <- rand_str()
+
+  x <- serialize(runif(10), NULL)
+  con$SET(key, x)
+
+  sha <- con$SCRIPT_LOAD(sprintf("return redis.call('get', '%s')", key))
+  expect_identical(con$EVALSHA(sha, 1, key, NULL), x)
+
+  expect_equal(con$SCRIPT_EXISTS(sha), list(1))
+
+  expect_equal(con$SCRIPT_FLUSH(), redis_status("OK"))
+
+    expect_equal(con$SCRIPT_EXISTS(sha), list(0))
+})
+
+test_that("SCRIPT DEBUG", {
+  expect_equal(redis_cmds$SCRIPT_DEBUG("YES"),
+               list("SCRIPT", "DEBUG", "YES"))
+  expect_equal(redis_cmds$SCRIPT_DEBUG("NO"),
+               list("SCRIPT", "DEBUG", "NO"))
+})
+
+test_that("SCRIPT KILL", {
+  expect_equal(redis_cmds$SCRIPT_KILL(),
+               list("SCRIPT", "KILL"))
+})
