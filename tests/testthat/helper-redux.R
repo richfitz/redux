aa952f3cc2afd82dcec87098854646761653ea6a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-05-18 21:11:02 +0100

    Don't bail on startup with CRAN setup

--- tests/testthat/helper-redux.R
+++ tests/testthat/helper-redux.R
@@ -2,6 +2,7 @@ redis_cmds <- redis_api(list(command = identity))
 
 REDIS_VERSION <- tryCatch(
   redis_version(test_hiredis_connection()),
+  skip = function(e) numeric_version("0.0.0"),
   error = function(e) numeric_version("0.0.0"))
 REDIS_HOST <- redis_config()$host
 REDIS_PORT <- redis_config()$port
