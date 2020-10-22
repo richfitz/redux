c7ae9b6f8797d7187eec66dc83d1f7272b71fd2a
Jeroen Ooms <jeroenooms@gmail.com>
2020-10-22 13:17:21 +0200

    Windows: update to hiredis 1.0.0

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,6 +1,6 @@
-if (!file.exists("../windows/hiredis-0.9.2/include/hiredis/hiredis.h")) {
+if (!file.exists("../windows/hiredis-1.0.0/include/hiredis/hiredis.h")) {
   if (getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/hiredis/archive/v0.9.2.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/hiredis/archive/v1.0.0.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
