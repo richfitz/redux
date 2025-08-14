0360bb0141a2ba9efe9c7e92ce726879025d3f47
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-08-14 17:23:28 +0100

    More compatability fixes

--- tests/testthat/test-zzz-subscribe.R
+++ tests/testthat/test-zzz-subscribe.R
@@ -143,7 +143,7 @@ test_that("flood and recover", {
   res2 <- .Call(redux:::Credux_redis_unsubscribe, ptr, ch, pattern)
 
   expect_s3_class(res1, "try-error")
-  expect_equivalent(res2, list("unsubscribe", ch, 0L))
+  expect_equal(res2, list("unsubscribe", ch, 0L), ignore_attr = TRUE)
   expect_gt(attr(res2, "n_discarded"), 0)
 
   ## This one is important:
